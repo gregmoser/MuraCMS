2c2753ae6df40427cf8855c051feba3a88dd45df
Mat Levine <matt.levine@blueriver.com>
2013-04-19 09:04:04 -0700

    Fixing issue with setting template when creating component

--- admin/assets/js/architecture.js
+++ admin/assets/js/architecture.js
@@ -1206,6 +1206,8 @@ buttons: {
 						location.href = './';
 					}
 					$("#mura-quickEditor").html(data);
+					$("#mura-quickEditor").trigger('contextmenu');
+					
 					setDatePickers(".mura-quickEdit-datepicker", dtLocale, dtCh);
 					setToolTips(".mura-quickEdit-datepicker");
 					if($("#hasDraftsMessage").length) {
