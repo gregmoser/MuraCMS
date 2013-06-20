25268ba1bed2b71d01f9d6624934948a8c97500a
Mat Levine <matt.levine@blueriver.com>
2013-06-20 13:45:00 -0700

    Fixing issue with live/on switch

--- admin/assets/js/templatebuilder/jquery.templatebuilder.0.3.js
+++ admin/assets/js/templatebuilder/jquery.templatebuilder.0.3.js
@@ -184,7 +184,7 @@
 					goLoadField( jQuery(this).attr('data-object'),_formData.formid );
 				});
 			});
-			jQuery("#mura-tb-fields li div").live( 'click', function() {
+			jQuery(document).on( 'click', ,'#mura-tb-fields li div',function() {
 				doField( this );
 			});			
 		}
@@ -555,7 +555,12 @@
 				}
 			});
 			
-			jQuery(".mura-tb-grid-radio",$_grid).live('click',function() {
+			jQuery(document).on('click','.mura-tb-grid-radio',function() {
+				id = jQuery(this).attr('data-id');
+				_currentDataset.defaultid = id;
+			});
+
+			jQuery(document).on('click',$_grid,function() {
 				id = jQuery(this).attr('data-id');
 				_currentDataset.defaultid = id;
 			});
